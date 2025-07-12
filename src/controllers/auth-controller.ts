aee552eca04c16523c37f7103157dd0a489b7e1b
TallapallyAneesh <ta22mmb0a45@student.nitw.ac.in>
2025-07-12 23:39:58 +0530

    change cookie settings

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -114,7 +114,7 @@ export const registerUser = async (
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(201).json({
@@ -193,7 +193,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
